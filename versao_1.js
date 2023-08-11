909e4ba25c2c2eec89c04b3748519cde6b018f26
CaioKN7 <galdinocaiost@gmail.com>
2023-08-11 17:41:24 -0300

    Blackjack versão_1

--- versao_1.js
+++ versao_1.js
@@ -10,3 +10,37 @@
  * 
  * 
  */
+
+console.log("Bem-vindo(a) ao jogo de blackjack")
+
+let iniciarJogo = confirm("Quer iniciar uma nova rodada?")
+
+if (iniciarJogo) {
+   let primeiraCartaUsuario = comprarCarta();
+   let segundaCartaUsuario = comprarCarta();
+   let primeiraCartaMaquina = comprarCarta();
+   let segundaCartaMaquina = comprarCarta();
+
+   //console.log(primeiraCartaUsuario);
+
+
+  let pontosUsuario = primeiraCartaUsuario.valor+segundaCartaUsuario.valor
+  let pontosMaquina = primeiraCartaMaquina.valor+segundaCartaMaquina.valor
+
+   console.log(`Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto} - ${pontosUsuario}`);
+   console.log(`Máquina - cartas: ${primeiraCartaMaquina.texto} ${segundaCartaMaquina.texto} - ${pontosMaquina}`);
+
+   if (pontosUsuario>pontosMaquina) {
+      console.log('Parabéns, você ganhou! (Mas não acostuma -_-)');
+      
+   } else if(pontosMaquina>pontosUsuario) {
+      console.log("A máquina ganhou (Olha minha revolução chegando aí, hein O_o)");
+   } else if (pontosUsuario===pontosMaquina){
+      console.log("EMPATE");
+   } 
+   
+} else { 
+   console.log("O jogo acabou");
+}
+
+
